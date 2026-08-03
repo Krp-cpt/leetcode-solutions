7f833bfa1dce0b6804c2441d2d1e8a8e8eb140e2
Krp-cpt <kartikpadavalkar04@gmail.com>
2026-08-03 23:03:55 +0530

    Time: 85 ms (5.95%), Space: 47 MB (59.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        int n=0,m=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=nums.length-1;j>0;j--)
+            {
+                if(i!=j && target == nums[i] + nums[j])
+                {
+                    n=i;
+                    m=j;
+                    break;
+                }
+            }
+        }
+        return new int[]{n,m};
     }
 }
